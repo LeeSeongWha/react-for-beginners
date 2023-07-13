1f2e0155375444eaa512ea71a02a4c6045f1cf53
LeeSeongWha <fpdlcm0614@gmail.com>
2023-07-13 17:44:41 +0900

    123

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 import { element } from 'prop-types';
