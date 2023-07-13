ff36abf5632fd30618920a6bb024ce8dfb886295
LeeSeongWha <fpdlcm0614@gmail.com>
2023-07-13 17:39:28 +0900

    fdfd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
+import { element } from 'prop-types';
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movie/:id">
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id`}>
           <Detail />
         </Route>
-        <Route path="/">
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}>
           <Home />
         </Route>
       </Switch>
