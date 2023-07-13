bd1cbcc778bdfb63df4a72000fc5e5529906f4bc
LeeSeongWha <fpdlcm0614@gmail.com>
2023-07-13 18:38:49 +0900

    3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 import { element } from 'prop-types';
@@ -6,10 +6,10 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movie/:id">
-          <Detail />
+        <Route>
+          <Detail path={`${process.env.PUBLIC_URL}/movie/:id`} />
         </Route>
-        <Route path="/">
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}>
           <Home />
         </Route>
       </Switch>
